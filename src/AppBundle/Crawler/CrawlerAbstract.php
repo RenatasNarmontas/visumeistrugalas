17ac17ce30972b0ecfa1354380bbc81e268386ce
Renatas Narmontas <renatas.narmontas@gmail.com>
2016-03-31 00:59:17 +0300

    Refactor forecasts entities. Add missing annotations

--- src/AppBundle/Crawler/CrawlerAbstract.php
+++ src/AppBundle/Crawler/CrawlerAbstract.php
@@ -9,12 +9,8 @@
 namespace AppBundle\Crawler;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-//use AppBundle\Entity\Temperatures;
-use AppBundle\Entity\Forecasts1d;
-use AppBundle\Entity\Forecasts2d;
-use AppBundle\Entity\Forecasts3d;
-use AppBundle\Entity\Forecasts4d;
-use AppBundle\Entity\Forecasts5d;
+//use AppBundle\Entity\Temperature;
+use AppBundle\Entity\Forecast;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -39,59 +35,12 @@ abstract class CrawlerAbstract extends ContainerAwareCommand implements CrawlerI
     }
 
     /**
-     * Inserts forecast values to 'forecasts_1d' table
+     * Inserts forecast values to 'forecasts' table
      */
-    public function insertForecast1dTemperature(Forecasts1d $forecasts1d)
+    public function insertForecastTemperature(Forecast $forecast)
     {
         $em = $this->getContainer()->get('doctrine')->getManager();
-        $em->persist($forecasts1d);
+        $em->persist($forecast);
         $em->flush();
     }
-
-    /**
-     * Inserts forecast values to 'forecasts_2d' table
-     */
-    public function insertForecast2dTemperature(Forecasts2d $forecasts2d)
-    {
-        $em = $this->getContainer()->get('doctrine')->getManager();
-        $em->persist($forecasts2d);
-        $em->flush();
-    }
-
-    /**
-     * Inserts forecast values to 'forecasts_3d' table
-     */
-    public function insertForecast3dTemperature(Forecasts3d $forecasts3d)
-    {
-        $em = $this->getContainer()->get('doctrine')->getManager();
-        $em->persist($forecasts3d);
-        $em->flush();
-    }
-
-    /**
-     * Inserts forecast values to 'forecasts_4d' table
-     */
-    public function insertForecast4dTemperature(Forecasts4d $forecasts4d)
-    {
-        $em = $this->getContainer()->get('doctrine')->getManager();
-        $em->persist($forecasts4d);
-        $em->flush();
-    }
-
-    /**
-     * Inserts forecast values to 'forecasts_5d' table
-     */
-    public function insertForecast5dTemperature(Forecasts5d $forecasts5d)
-    {
-        $em = $this->getContainer()->get('doctrine')->getManager();
-        $em->persist($forecasts5d);
-        $em->flush();
-    }
-
-    /**
-     * Logger
-     *
-     * @param string $message
-     */
-    abstract protected function logger(string $message);
 }
