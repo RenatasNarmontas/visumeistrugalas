624e397b18d3e4c6f8b6082e662614a3ccae094b
Marina Udalova <marina.udalova92@gmail.com>
2016-03-31 19:37:50 +0300

    Add namespace and Fix classname

--- src/AppBundle/DependencyInjection/OpenWeatherMapExtension.php
+++ src/AppBundle/DependencyInjection/OpenWeatherMapExtension.php
@@ -1,12 +1,13 @@
 <?php
 
+namespace AppBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class OpenWeatherMapExtensione extends Extension
+class OpenWeatherMapExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
