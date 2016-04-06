0eeb3ddd3782821c198be9a9fb8a291de34f3b03
Vilius Krikščionaitis <viliusk31@gmail.com>
2016-04-06 14:11:28 +0300

    Add Yahoo Crawler and Request entity

--- src/AppBundle/Command/YahooCrawlerCommand.php
+++ src/AppBundle/Command/YahooCrawlerCommand.php
@@ -35,9 +35,7 @@ EOF
         $cityName = $this->normalizeCityName($input->getOption('city'));
 
 
-        $json_string = file_get_contents(
-            'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'.$cityName.'%22)%20and%20u%3D%22C%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-        );
+        $json_string = file_get_contents('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'.$cityName.'%22)%20and%20u%3D%22C%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys');
         $parsed_json = json_decode($json_string);
 
         $city = $parsed_json->query->results->channel->location->city;
