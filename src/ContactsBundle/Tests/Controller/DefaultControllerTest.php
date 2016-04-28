a0cb61fdbf25d91d036d58d07f2eec97fb8b7408
Renatas Narmontas <renatas.narmontas@gmail.com>
2016-04-28 22:27:25 +0300

    Remove unused repository; Fix default test

--- src/ContactsBundle/Tests/Controller/DefaultControllerTest.php
+++ src/ContactsBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,8 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/contacts');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertContains('Contacts', $client->getResponse()->getContent());
     }
 }
