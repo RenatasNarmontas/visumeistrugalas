493d45e8cb87b4c5df5235658e21fd97aba12ff3
Renatas Narmontas <renatas.narmontas@gmail.com>
2016-05-02 04:00:30 +0300

    Add missing RequestRepository

---
+++ src/AppBundle/Repository/RequestRepository.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Renatas Narmontas
+ * Date: 02/05/16
+ * Time: 03:48
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * RequestRepository
+ */
+class RequestRepository extends EntityRepository
+{
+}
